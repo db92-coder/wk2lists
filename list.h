98a8b4b1e760a6641ade8a925d49ae5d48e32e91
db92-coder <danielbroadby92@gmail.com>
2023-03-01 20:02:05 +1100

    adding merge and reverse function

--- list.h
+++ list.h
@@ -33,3 +33,6 @@ void delete_list(List* self, int data);
 
 // Function to free the memory allocated to a list and all its nodes.
 void destroy_list(List* self);
+
+List reverse(List list);
+List merge(List list1, List list2);
--- wk2main.c
+++ wk2main.c
@@ -12,13 +12,14 @@ int main() {
     int quit = 0;
     int option;
 
+
     do {
-        printf("option 0: QUIT | option 1: INSERT | option 2: DELETE | option 3: PRINT \n");
-        scanf_s("%d", &option);
+        printf("option 0: QUIT | option 1: INSERT | option 2: DELETE | option 3: PRINT | option 4: REVERSE | option 5: MERGE \n");
+        scanf("%d", &option);
 
         switch (option) {
             case 0:
-                quit = 0;
+                quit = 1;
                 break;
             case 1:
                 option_insert(&my_list);
@@ -28,6 +29,36 @@ int main() {
                 break;
             case 3:
                 option_print(&my_list);
+                break;
+            case 4:
+                my_list = reverse(my_list);
+                break;
+            case 5:
+            {
+                List list1 = new_list();
+                List list2 = new_list();
+                printf("Enter elements of the first list (in ascending order, -1 to stop):\n");
+                int element;
+                do {
+                    scanf("%d", &element);
+                    if (element != -1) {
+                        insert_in_order(&list1, element);
+                    }
+                } while (element != -1);
+                printf("Enter elements of the second list (in ascending order, -1 to stop):\n");
+                do {
+                    scanf("%d", &element);
+                    if (element != -1) {
+                        insert_in_order(&list2, element);
+                    }
+                } while (element != -1);
+                my_list = merge(list1, list2);
+                destroy_list(&list1);
+                destroy_list(&list2);
+            }
+                break;
+            default:
+                printf("Invalid option\n");
                 break;
         }
 
@@ -39,18 +70,62 @@ void option_insert(List *list)
 {
     int value;
     printf("Enter an integer to insert: ");
-    scanf_s("%d", &value);
+    scanf("%d", &value);
     insert_at_front(list, value);
 }
 void option_delete(List *list)
 {
     int value;
     printf("Enter an integer to delete: ");
-    scanf_s("%d", &value);
+    scanf("%d", &value);
     delete_list(list, value);
 }
 
 void option_print(List *list)
 {
     print_list(list);
+}
+
+// Function to reverse the given linked list.
+List reverse(List list) {
+    List reversed = new_list();
+    ListNodePtr current = list.head;
+
+    while (current != NULL) {
+        insert_at_front(&reversed, current->data);
+        current = current->next;
+    }
+
+    return reversed;
+}
+
+// Function to merge two given linked lists.
+List merge(List list1, List list2) {
+    List merged = new_list();
+    ListNodePtr current1 = list1.head;
+    ListNodePtr current2 = list2.head;
+
+    while (current1 != NULL && current2 != NULL) {
+        if (current1->data < current2->data) {
+            insert_at_front(&merged, current1->data);
+            current1 = current1->next;
+        }
+        else {
+            insert_at_front(&merged, current2->data);
+            current2 = current2->next;
+        }
+    }
+
+    // Append the remaining nodes from list1 or list2
+    while (current1 != NULL) {
+        insert_at_front(&merged, current1->data);
+        current1 = current1->next;
+    }
+
+    while (current2 != NULL) {
+        insert_at_front(&merged, current2->data);
+        current2 = current2->next;
+    }
+
+    return merged;
 }
